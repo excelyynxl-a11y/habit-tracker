897b0f9fe93f8346487bd58a2288f245627f3f1c
Excelyynx <excelyynxl@gmail.com>
2026-01-19 13:03:30 +0800

    setup native-react

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.view}
+    >
+      <Text>oiuouoiuoiuoioo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  navButton: {
+    width: 100,
+    height: 20,
+    backgroundColor: "coral",
+    borderRadius: 8,
+    textAlign: "center",
+  }
+})
