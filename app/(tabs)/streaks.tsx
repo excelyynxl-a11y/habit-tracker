49e0cdc861538549b1788c3dd01c7b44f56d7cd7
Excelyynx <excelyynxl@gmail.com>
2026-01-20 14:00:19 +0800

    initialise empty streaks page

---
+++ app/(tabs)/streaks.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function StreakScreen() {
+    return (
+        <View>
+            <Text>Streak Page </Text>
+        </View>
+    )
+}
